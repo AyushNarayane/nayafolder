8e39dedf9052a63ef6b0bac7e1bb44dbc4d7831c
AyushNarayane <ayushnarayane12@gmial.com>
2025-04-08 23:46:30 +0530

    fix : header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,24 @@
 import { Link } from 'react-router-dom';
-import { useCart } from '../contexts/CartContext';
 import './Header.css';
 
 const Header = ({ onLogout }) => {
-  const { cartCount } = useCart();
-
   return (
     <header className="header">
-      <div className="container header-container">
-        <Link to="/" className="logo">ShopNow</Link>
+      <div className="header-content">
+        <Link to="/" className="logo">
+          Naya Shop
+        </Link>
         
-        <nav className="nav">
-          <ul className="nav-list">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/cart" className="nav-link cart-link">
-                Cart
-                {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <button onClick={onLogout} className="nav-link logout-btn">Logout</button>
-            </li>
-          </ul>
+        <nav className="nav-links">
+          <Link to="/" className="nav-link">
+            Products
+          </Link>
+          <Link to="/cart" className="nav-link">
+            Cart
+          </Link>
+          <button onClick={onLogout} className="logout-btn">
+            Logout
+          </button>
         </nav>
       </div>
     </header>
